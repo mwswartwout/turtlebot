ec3f59f2d342598d416b7a73ee194b4dd68a6bd0
Matthew Swartwout <mwswartwout@gmail.com>
2016-05-04 18:28:30 -0400

    More changes?

--- src/turtlebot/sensor_record.py
+++ src/turtlebot/sensor_record.py
@@ -5,15 +5,20 @@ import csv
 from nav_msgs.msg import Odometry
 
 def odom_callback(new_odom):
+    pose_x = new_odom.pose.pose.position.x
+    pose_y = new_odom.pose.pose.position.y
     pose_covariance = new_odom.pose.covariance
-    with open('odometry_filtered_pose_modified.csv', 'a') as pose_file:
-        writer = csv.writer(pose_file)
-        writer.writerow(pose_covariance)
-
     twist_covariance = new_odom.twist.covariance
-    with open('odometry_filtered_twist_modified.csv', 'a') as twist_file:
-        writer = csv.writer(twist_file)
-        writer.writerow(twist_covariance)
+    data = [pose_x, pose_y, (''.join(str(pose_covariance))).split(","), (''.join(str(twist_covariance))).split(",")]
+
+    with open('odometry_filtered.csv', 'a') as pose_file:
+        writer = csv.writer(pose_file)
+        writer.writerow(data)
+
+    # twist_covariance = new_odom.twist.covariance
+    # with open('odometry_filtered_twist_modified.csv', 'a') as twist_file:
+    #    writer = csv.writer(twist_file)
+    #    writer.writerow(twist_covariance)
 
 if __name__ == '__main__':
     try:
